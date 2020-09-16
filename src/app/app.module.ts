36781af8bb47dd57ad7ce7a238fa2f3dc7a6555c
Cristian Araujo <araujocristian@live.com>
2020-09-16 20:42:50 -0300

    Criando Componente Início (Home)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component'
+import { HomeComponent } from './views/home/home.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -24,6 +27,7 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
+    MatCardModule,
     MatListModule
   ],
   providers: [],
